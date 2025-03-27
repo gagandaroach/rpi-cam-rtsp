95af14d4fde29fcf10748eeb96fd92c615bac5a0
Gagan Daroach <gagandaroach@gmail.com>
2025-03-27 06:35:01 -0700

    Update cam-ffmpeg-stream.sh

--- cam-ffmpeg-stream.sh
+++ cam-ffmpeg-stream.sh
@@ -1,1 +1,2 @@
-ffmpeg -f v4l2 -video_size 640x480 -framerate 30 -input_format mjpeg -i /dev/video0 -c:v libx264 -preset ultrafast -tune zerolatency -f rtsp rtsp://0.0.0.0:8554/video0
+# ffmpeg -f v4l2 -video_size 640x480 -framerate 30 -input_format mjpeg -i /dev/video0 -c:v libx264 -preset ultrafast -tune zerolatency -f rtsp rtsp://0.0.0.0:8554/video0
+ffmpeg -f v4l2 -video_size 640x480 -framerate 30 -input_format mjpeg -i /dev/video0 -c:v libx264 -preset ultrafast -tune zerolatency -f rtsp rtsp://daroach.lan:8554/3dprinter
